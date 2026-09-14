ed3b4ccf219080b989620e2097720eff9cd939a1
wanjiruaisha <wanjiruaisha30@gmail.com>
2026-09-14 22:58:35 +0300

    feat: add login and token refresh endpoints to user URLs

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from .views import CurrentUserView, RegisterView
+from .views import CurrentUserView, RegisterView, LoginView, RefreshView
+
+
+
 
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("me/", CurrentUserView.as_view(), name="current_user"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("refresh/", RefreshView.as_view(), name="token_refresh"),
+
 
 ]
