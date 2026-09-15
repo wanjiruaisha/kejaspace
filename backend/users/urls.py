ee1bec3003e51e75ce4494b614c5f519ed0eea0a
wanjiruaisha <wanjiruaisha30@gmail.com>
2026-09-15 08:42:48 +0300

    feat: add LogoutView to user URL patterns

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import CurrentUserView, RegisterView, LoginView, RefreshView
+from .views import CurrentUserView, LogoutView, RegisterView, LoginView, RefreshView
 
 
 
@@ -11,6 +11,7 @@ urlpatterns = [
     path("me/", CurrentUserView.as_view(), name="current_user"),
     path("login/", LoginView.as_view(), name="login"),
     path("refresh/", RefreshView.as_view(), name="token_refresh"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 
 
 ]
