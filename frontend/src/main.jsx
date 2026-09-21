66e73c999cf3fa17a1c3811c9a026805bb6e1c3b
wanjiruaisha <wanjiruaisha30@gmail.com>
2026-09-21 11:58:06 +0300

    feat: wrap App component with AuthProvider for authentication context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
+import AuthProvider from "./context/AuthContext";
+
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
