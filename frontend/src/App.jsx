207b1d8a0e9a7b2a509e8768952f1bbde9628657
wanjiruaisha <wanjiruaisha30@gmail.com>
2026-09-22 19:30:47 +0300

    feat: reorganize routes in App component for better structure and clarity

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import { Route, Routes } from "react-router";
 
 import PublicLayout from "./layouts/PublicLayout";
+
 import HomePage from "./pages/HomePage";
 import RoomsPage from "./pages/RoomsPage";
-import NotFoundPage from "./pages/NotFoundPage";
 import RoomDetailsPage from "./pages/RoomDetailsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import UnauthorizedPage from "./pages/UnauthorizedPage";
+
 import RegisterPage from "./pages/auth/RegisterPage";
 import LoginPage from "./pages/auth/LoginPage";
+import MyApplicationsPage from "./pages/resident/MyApplicationsPage";
+
+import ProtectedRoute from "./routes/ProtectedRoute";
+import RoleRoute from "./routes/RoleRoute";
 
 export default function App() {
   return (
@@ -15,8 +22,20 @@ export default function App() {
         <Route index element={<HomePage />} />
         <Route path="rooms" element={<RoomsPage />} />
         <Route path="rooms/:id" element={<RoomDetailsPage />} />
+
         <Route path="register" element={<RegisterPage />} />
         <Route path="login" element={<LoginPage />} />
+        <Route path="unauthorized" element={<UnauthorizedPage />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route element={<RoleRoute allowedRoles={["resident"]} />}>
+            <Route
+              path="my-applications"
+              element={<MyApplicationsPage />}
+            />
+          </Route>
+        </Route>
+
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
